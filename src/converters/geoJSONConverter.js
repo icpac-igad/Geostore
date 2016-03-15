720a497a95d683ceb3a32f07acd4040f49454cb5
rrequero <rareq1987@gmail.com>
2016-03-15 15:31:24 +0100

    add change in converter

--- src/converters/geoJSONConverter.js
+++ src/converters/geoJSONConverter.js
@@ -3,7 +3,16 @@
 let logger = require('logger');
 
 module.exports.convert = function(data){
-    if(data.coordinates){
+    if(data.type === 'FeatureCollection'){
+        logger.debug('Is a FeatureCollection');
+        return data;
+    } else if(data.type === 'Feature'){
+        logger.debug('Is a feature');
+        return {
+            type: 'FeatureCollection',
+            features:[data]
+        };
+    } else{
         logger.debug('is a geometry');
         return {
             type: 'FeatureCollection',
@@ -12,14 +21,5 @@ module.exports.convert = function(data){
                 geometry: data
             }]
         };
-    } else if(data.geometry){
-        logger.debug('Is a feature');
-        return {
-            type: 'FeatureCollection',
-            features:[data]
-        };
-    } else{
-        logger.debug('Is a featureCollection');
-        return data;
     }
 };
