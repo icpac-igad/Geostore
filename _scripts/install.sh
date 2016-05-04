017b65341c749f43e935d50f2b77d00d0e76af14
rrequero <rareq1987@gmail.com>
2016-05-04 18:30:47 +0200

    add config

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -3,7 +3,7 @@ set -x # Show the output of the following commands (useful for debugging)
 
 ssh-keyscan -H $DEPLOY_HOST >> $HOME/.ssh/known_hosts
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_2a275d539911_key -iv $encrypted_2a275d539911_iv -in deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $encrypted_0f765e1d606f_key -iv $encrypted_0f765e1d606f_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
