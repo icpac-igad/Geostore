d8bb58fcb864e2ad6cdf06692916fca10cee4e62
rrequero <rareq1987@gmail.com>
2016-07-20 17:09:54 +0200

    remove grunt-run

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -9,18 +9,6 @@ module.exports = function(grunt) {
 
 
         clean: {},
-
-        run: {
-            options: {
-                // Task-specific options go here.
-            },
-            migration: {
-                cmd: 'node',
-                args: [
-                    'app/src/migrate/migrate.js'
-                ]
-            }
-        },
         jshint: {
             js: {
                 src: [
