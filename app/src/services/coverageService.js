d890d979e2c7044743f8105de2b2b3b221e22c74
rrequero <rareq1987@gmail.com>
2016-11-02 11:29:00 +0100

    fix coverage feature

--- app/src/services/coverageService.js
+++ app/src/services/coverageService.js
@@ -1,13 +1,20 @@
 'use strict';
 var logger = require('logger');
-var CartoDBService = require('services/cartoDBService');
 var CoverageDuplicated = require('errors/coverageDuplicated');
 var CoverageNotFound = require('errors/coverageNotFound');
+var CartoDB = require('cartodb');
+var Mustache = require('mustache');
+var config = require('config');
 
-const ISO = `SELECT slug FROM coverage_layers cl, gadm2_countries_simple c where  ST_INTERSECTS(cl.the_geom, c.the_geom) and c.iso = UPPER('{{iso}}')`;
+const ISO = `with c as (select the_geom_webmercator, st_area(the_geom_webmercator)/10000 as area_ha from gadm2_countries_simple where iso = UPPER('{{iso}}')),
+            cl as (select (st_buffer(st_simplify(the_geom_webmercator,10000),1)) the_geom_webmercator, slug, coverage_slug from coverage_layers)
+            SELECT slug FROM  cl inner join c on ST_INTERSECTS(c.the_geom_webmercator, cl.the_geom_webmercator)
+            where (((st_area(ST_intersection(c.the_geom_webmercator, cl.the_geom_webmercator))/10000)::numeric/area_ha::numeric)*100)>1`;
 
-const ID1 = `SELECT slug FROM coverage_layers cl, gadm2_provinces_simple c where ST_INTERSECTS(cl.the_geom, c.the_geom) and c.iso = UPPER('{{iso}}')
-          AND c.id_1 = {{id1}}`;
+const ID1 = `with c as (select the_geom_webmercator, st_area(the_geom_webmercator)/10000 as area_ha from gadm2_provinces_simple where iso = UPPER('{{iso}}')) AND id_1 = {{id1}},
+            cl as (select (st_buffer(st_simplify(the_geom_webmercator,10000),1)) the_geom_webmercator, slug, coverage_slug from coverage_layers)
+            SELECT slug FROM  cl inner join c on ST_INTERSECTS(c.the_geom_webmercator, cl.the_geom_webmercator)
+            where (((st_area(ST_intersection(c.the_geom_webmercator, cl.the_geom_webmercator))/10000)::numeric/area_ha::numeric)*100)>1`;
 
 const WDPA = `with p as (SELECT p.the_geom AS the_geom
         FROM (
@@ -31,95 +38,107 @@ const WORLD = `SELECT slug FROM coverage_layers where ST_INTERSECTS(the_geom, ST
 
 
 var executeThunk = function(client, sql, params) {
-    return function(callback) {
-        client.execute(sql, params).done(function(data) {
-            callback(null, data);
-        }).error(function(err) {
-            callback(err[0], null);
-        });
-    };
+  return function(callback) {
+    client.execute(sql, params).done(function(data) {
+      callback(null, data);
+    }).error(function(err) {
+      callback(err[0], null);
+    });
+  };
 };
 
 
 class CoverageService {
 
-    * getNational(iso) {
-        logger.debug('Obtaining national of iso %s', iso);
-        let params = {
-            iso: iso
-        };
+  constructor() {
+    this.client = new CartoDB.SQL({
+      user: config.get('cartoDB.user')
+    });
+  }
 
-        let data = yield executeThunk(this.client, ISO, params);
-        if (data.rows && data.rows.length > 0) {
-            return data.rows.map( item => item.slug );
-        }
-        return [];
+  *
+  getNational(iso) {
+    logger.debug('Obtaining national of iso %s', iso);
+    let params = {
+      iso: iso
+    };
+
+    let data = yield executeThunk(this.client, ISO, params);
+    if (data.rows && data.rows.length > 0) {
+      return data.rows.map(item => item.slug);
     }
+    return [];
+  }
 
-    * getSubnational(iso, id1) {
-        logger.debug('Obtaining subnational of iso %s and id1', iso, id1);
-        let params = {
-            iso: iso,
-            id1: id1
-        };
+  *
+  getSubnational(iso, id1) {
+    logger.debug('Obtaining subnational of iso %s and id1', iso, id1);
+    let params = {
+      iso: iso,
+      id1: id1
+    };
 
-        let data = yield executeThunk(this.client, ID1, params);
-        if (data.rows && data.rows.length > 0) {
-            return data.rows.map( item => item.slug );
-        }
-        return [];
+    let data = yield executeThunk(this.client, ID1, params);
+    if (data.rows && data.rows.length > 0) {
+      return data.rows.map(item => item.slug);
     }
+    return [];
+  }
 
-    * getUse(useTable, id) {
-        logger.debug('Obtaining use with id %s', id);
+  *
+  getUse(useTable, id) {
+    logger.debug('Obtaining use with id %s', id);
 
-        let params = {
-            useTable: useTable,
-            pid: id
-        };
+    let params = {
+      useTable: useTable,
+      pid: id
+    };
 
-        let data = yield executeThunk(this.client, USE, params);
+    let data = yield executeThunk(this.client, USE, params);
 
-        if (data.rows && data.rows.length > 0) {
-            return data.rows.map( item => item.slug );
-        }
-        return [];
+    if (data.rows && data.rows.length > 0) {
+      return data.rows.map(item => item.slug);
     }
+    return [];
+  }
 
-    * getWdpa(wdpaid) {
-        logger.debug('Obtaining wpda of id %s', wdpaid);
+  *
+  getWdpa(wdpaid) {
+    logger.debug('Obtaining wpda of id %s', wdpaid);
 
-        let params = {
-            wdpaid: wdpaid
-        };
+    let params = {
+      wdpaid: wdpaid
+    };
 
-        let data = yield executeThunk(this.client, WDPA, params);
-        if (data.rows && data.rows.length > 0) {
-            return data.rows.map( item => item.slug );
-        }
-        return [];
+    let data = yield executeThunk(this.client, WDPA, params);
+    if (data.rows && data.rows.length > 0) {
+      return data.rows.map(item => item.slug);
     }
+    return [];
+  }
 
-    * getCoverages() {
-        logger.info('Getting coverages');
+  *
+  getCoverages() {
+    logger.info('Getting coverages');
 
-        let data = yield executeThunk(this.client, COVERAGES);
-        return data;
+    let data = yield executeThunk(this.client, COVERAGES);
+    return data;
+  }
+
+  *
+  getWorld(geojson) {
+    logger.info('Getting layers that intersect');
+
+    let params = {
+      geojson: JSON.stringify(geojson)
+    };
+
+    let data = yield executeThunk(this.client, WORLD, params);
+    if (data.rows && data.rows.length > 0) {
+      return data.rows.map(item => item.slug);
     }
-
-    * getWorld(geojson) {
-        logger.info('Getting layers that intersect');
-
-        let params = {
-            geojson: JSON.stringify(geojson)
-        };
-
-        let data = yield executeThunk(this.client, WORLD, params);
-        if (data.rows && data.rows.length > 0) {
-            return data.rows.map( item => item.slug );
-        }
-        return [];
-    }
+    return [];
+  }
 }
 
-module.exports = CoverageService;
+module.exports = new CoverageService();
