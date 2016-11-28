4e1b0cf6d38bfcdb3b864b6e08d54d2ffb3c130a
rrequero <rareq1987@gmail.com>
2016-11-28 12:56:30 +0100

    refactor to new register of ct

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 //load modules
-if(process.env.NODE_ENV === 'prod'){
-    require('newrelic');
+if (process.env.NODE_ENV === 'prod') {
+  require('newrelic');
 }
 var config = require('config');
 var logger = require('logger');
@@ -15,79 +15,77 @@ var validate = require('koa-validate');
 var mongoose = require('mongoose');
 var ErrorSerializer = require('serializers/errorSerializer');
 var mongoUri = process.env.MONGO_URI || 'mongodb://' + config.get('mongodb.host') + ':' + config.get('mongodb.port') + '/' + config.get('mongodb.database');
-var registerClient = require('vizz.microservice-client');
+const ctRegisterMicroservice = require('ct-register-microservice-node');
 
 var onDbReady = function(err) {
-    if (err) {
-        logger.error(err);
-        throw new Error(err);
+  if (err) {
+    logger.error(err);
+    throw new Error(err);
+  }
+
+  // instance of koa
+  var app = koa();
+
+  //if environment is dev then load koa-logger
+  if (process.env.NODE_ENV === 'dev') {
+    app.use(koaLogger());
+  }
+
+  app.use(bodyParser({
+    jsonLimit: '50mb'
+  }));
+
+  //catch errors and send in jsonapi standard. Always return vnd.api+json
+  app.use(function*(next) {
+    try {
+      yield next;
+    } catch (err) {
+      logger.error(err);
+      this.status = err.status || 500;
+      this.body = ErrorSerializer.serializeError(this.status, err.message || err);
+      logger.debug(this.body);
+      if (process.env.NODE_ENV === 'prod' && this.status === 500) {
+        this.body = 'Unexpected error';
+      }
     }
+    this.response.type = 'application/vnd.api+json';
+  });
 
-    // instance of koa
-    var app = koa();
+  //load custom validator
+  require('validators/geoJSONValidator');
+  app.use(validate());
 
-    //if environment is dev then load koa-logger
-    if (process.env.NODE_ENV === 'dev') {
-        app.use(koaLogger());
-    }
+  //load routes
+  loader.loadRoutes(app);
 
-    app.use(bodyParser({
-        jsonLimit: '50mb'
-    }));
+  //Instance of http module
+  var server = require('http').Server(app.callback());
 
-    //catch errors and send in jsonapi standard. Always return vnd.api+json
-    app.use(function*(next) {
-        try {
-            yield next;
-        } catch (err) {
-            logger.error(err);
-            this.status = err.status || 500;
-            this.body = ErrorSerializer.serializeError(this.status, err.message || err);
-            logger.debug(this.body);
-            if (process.env.NODE_ENV === 'prod' && this.status === 500) {
-                this.body = 'Unexpected error';
-            }
-        }
-        this.response.type = 'application/vnd.api+json';
+
+
+  // get port of environment, if not exist obtain of the config.
+  // In production environment, the port must be declared in environment variable
+  var port = process.env.PORT || config.get('service.port');
+
+  server.listen(port, function() {
+    ctRegisterMicroservice.register({
+      info: require('../microservice/register.json'),
+      swagger: require('../microservice/public-swagger.json'),
+      mode: process.env.NODE_ENV === 'dev' ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
+      framework: ctRegisterMicroservice.KOA1,
+      app,
+      logger,
+      name: config.get('service.name'),
+      ctUrl: process.env.CT_URL,
+      url: process.env.LOCAL_URL,
+      active: true,
+    }).then(() => {}, (err) => {
+      logger.error(err);
+      process.exit(1);
     });
+  });
 
-    //load custom validator
-    require('validators/geoJSONValidator');
-    app.use(validate());
-
-    //load routes
-    loader.loadRoutes(app);
-
-    registerClient.register({
-        id: config.get('service.id'),
-        name: config.get('service.name'),
-        uri: config.get('service.uri'),
-        dirConfig: path.join(__dirname, '../microservice'),
-        dirPackage: path.join(__dirname, '../../'),
-        logger: logger,
-        app: app
-    });
-    //Instance of http module
-    var server = require('http').Server(app.callback());
-
-
-
-    // get port of environment, if not exist obtain of the config.
-    // In production environment, the port must be declared in environment variable
-    var port = process.env.PORT || config.get('service.port');
-
-    server.listen(port, function() {
-        if (process.env.NODE_ENV === 'dev'){
-            co(function *(){
-                yield registerClient.autoDiscovery();
-            }).then(function(){}, function(e){
-                logger.error('Error auto discovery', e);
-                process.exit(1);
-            });
-        }
-    });
-
-    logger.info('Server started in port:' + port);
+  logger.info('Server started in port:' + port);
 
 };
 
