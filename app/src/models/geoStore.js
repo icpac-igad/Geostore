bd600c29e3eb6fb5fee38cf5c5ffff03246ebd4c
rrequero <rrequero@gmail.com>
2017-11-30 18:50:50 +0100

    update index

--- app/src/models/geoStore.js
+++ app/src/models/geoStore.js
@@ -43,6 +43,10 @@ var GeoStore = new Schema({
     }
 });
 
-GeoStore.index({hash: 1, "info.iso": 1, "info.name": 1, "info.id1": 1, "info.wdpaid": 1, "info.use.use": 1, "info.use.id": 1});
+GeoStore.index({hash: 1, 'info.iso': 1, 'info.name': 1, 'info.id1': 1, 'info.wdpaid': 1, 'info.use.use': 1, 'info.use.id': 1});
+GeoStore.index({'info.iso': 1});
+GeoStore.index({'info.iso': 1, 'info.id1': 1});
+GeoStore.index({'info.wdpaid': 1});
+GeoStore.index({'info.use.use': 1, 'info.use.id': 1});
 
 module.exports = mongoose.model('GeoStore', GeoStore);
