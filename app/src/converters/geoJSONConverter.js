e53470a91f50c8e1ed6e3031911d77b187e954f7
Thomas.Maschler <tmaschler@wri.org>
2018-05-16 20:00:08 -0500

    Revert "repair geometry"
    This reverts commit 3fb4e04587958a41dffbc283d9003ae250f339d7.

--- app/src/converters/geoJSONConverter.js
+++ app/src/converters/geoJSONConverter.js
@@ -2,52 +2,25 @@
 
 let logger = require('logger');
 
-module.exports.makeFeatureCollection = function(data){
+module.exports.convert = function(data){
     if(data.type === 'FeatureCollection'){
         logger.debug('Is a FeatureCollection');
-        data.features.properties = null;
         return data;
     } else if(data.type === 'Feature'){
         logger.debug('Is a feature');
-        data.properties = null;
         return {
             type: 'FeatureCollection',
-            features:[data],
-           /*crs: { 
-                    type: 'name', 
-                    properties: { 
-                        name: 'urn:ogc:def:crs:OGC:1.3:CRS84' 
-                    } 
-            }*/
+            features:[data]
         };
     } else{
-        logger.debug('Is a geometry');
+        logger.debug('is a geometry');
         return {
             type: 'FeatureCollection',
             features:[{
                 type: 'Feature',
-                properties: null,
+                properties: {},
                 geometry: data
-            }],
-          /*  crs: { 
-                    type: 'name', 
-                    properties: { 
-                        name: 'urn:ogc:def:crs:OGC:1.3:CRS84' 
-                    } 
-            }*/
+            }]
         };
-    }
-};
-
-module.exports.getGeometry = function(data){
-    if(data.type === 'FeatureCollection'){
-        logger.debug('Is a FeatureCollection');
-        return data.features[0].geometry;
-    } else if(data.type === 'Feature'){
-        logger.debug('Is a feature');
-        return data.geometry;
-    } else{
-        logger.debug('Is a geometry');
-        return data;
     }
 };
