ab89affc5db4c344f159178f5a269231546bc7e6
Tiago Garcia <tiagojsag@gmail.com>
2019-03-10 08:29:13 +0100

    Fix unit tests for improved error reporting on invalid geometry type

--- app/test/unit/services/GeoStoreService2.test.js
+++ app/test/unit/services/GeoStoreService2.test.js
@@ -1,21 +1,13 @@
-'use strict';
-var logger = require('logger');
-var should = require('should');
-var assert = require('assert');
-var expect = require('chai').expect;
-var GeoStoreService2 = require('services/geoStoreService');
-var UnknownGeometry = require('errors/unknownGeometry');
+const GeoStoreService2 = require('services/geoStoreService');
 
-describe('GeoStore Service test', function() {
+describe('GeoStore Service test', function () {
 
-
-    var pointGeometry = {
+    const pointGeometry = {
         type: "Point",
         coordinates: [-1.6596221923828125, 50.91255835156951]
-     };
+    };
 
-
-    var polygonGeometry = {
+    const polygonGeometry = {
         type: 'Polygon',
         coordinates: [
             [
@@ -28,39 +20,29 @@ describe('GeoStore Service test', function() {
         ]
     };
 
-    var invalidGeometry = {
+    const invalidGeometry = {
         type: "Unknown",
         coordinates: [-1.6596221923828125, 50.91255835156951]
-     };
-
-    before(function*() {
-
-    });
+    };
 
     it('Get Point Geometry Type', function () {
-     let geometry_type = GeoStoreService2.getGeometryType(pointGeometry);
+        let geometry_type = GeoStoreService2.getGeometryType(pointGeometry);
         geometry_type.should.be.exactly(1).and.be.a.Number();
     });
 
 
     it('Get Polygon Geometry Type', function () {
-     let geometry_type = GeoStoreService2.getGeometryType(polygonGeometry);
+        let geometry_type = GeoStoreService2.getGeometryType(polygonGeometry);
         geometry_type.should.be.exactly(3).and.be.a.Number();
     });
 
     it('Test unknown geometry', function () {
-     try
-        {GeoStoreService2.getGeometryType(invalidGeometry)}
-     catch(err) {
-        var expected = 'Unknown geometry type'
-        assert.equal(err.message, expected)
+        try {
+            GeoStoreService2.getGeometryType(invalidGeometry)
+        } catch (err) {
+            const expected = 'Unknown geometry type: Unknown';
+            err.message.should.equal(expected);
         }
-
-    });
-
-
-
-    after(function*() {
 
     });
 });
